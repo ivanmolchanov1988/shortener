dbe3b3d32d88d4d00ec5e6896a7d6f4182ead4f3
iamolchan <iamolchan1@mts.ru>
2024-07-08 23:52:58 +0300

    Using chi in main.go and the TEMP tag for server.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	//"github.com/go-chi/chi/v5/middleware"
 	"github.com/ivanmolchanov1988/shortener/pkg/handlers"
 	"github.com/ivanmolchanov1988/shortener/pkg/memory"
-	"github.com/ivanmolchanov1988/shortener/pkg/server"
+	//"github.com/ivanmolchanov1988/shortener/pkg/server"
 )
 
 func main() {
 
 	memStore := memory.NewMemoryStorage()
 	handler := handlers.NewHandler(memStore)
-	srv := server.NewServer(handler)
+	//srv := server.NewServer(handler)
 
-	err := srv.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+	// err := srv.ListenAndServe()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	r := chi.NewRouter()
+	r.Post("/", handler.PostUrl)
+	r.Get("/{id}", handler.GetUrl)
+
+	http.ListenAndServe(":8080", r)
 }
