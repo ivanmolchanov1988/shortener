ed80919d43b86e78a417d3f0af2b0af5d8c0b3e5
iamolchan <iamolchan1@mts.ru>
2024-07-09 14:58:47 +0300

    Added a func getAddressAndBaseURL in config/config.go for a switch beween ENV and flags #3

--- config/config.go
+++ config/config.go
@@ -26,14 +26,15 @@ func getAddressAndBaseURL() (string, string) {
 	address := os.Getenv("SERVER_ADDRESS")
 	baseURL := os.Getenv("BASE_URL")
 
+	tempAddress := flag.String("a", "localhost:8080", "address to start the HTTP server")
+	tempBaseURL := flag.String("b", "http://localhost:8080", "the URL for the shortURL")
+
 	if address == "" {
-		tempAddress := flag.String("a", "localhost:8080", "address to start the HTTP server")
 		address = *tempAddress
 	} else {
 		fmt.Printf("Using ENV for address: %s\n", address)
 	}
 	if baseURL == "" {
-		tempBaseURL := flag.String("b", "http://localhost:8080", "the URL for the shortURL")
 		baseURL = *tempBaseURL
 	} else {
 		fmt.Printf("Using ENV for baseURL: %s\n", baseURL)
