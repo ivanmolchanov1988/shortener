1ed93d3efd5d12e83f1f3e33cf9f49c66f6b00d1
iamolchan <iamolchan1@mts.ru>
2024-11-24 06:18:05 +0300

    iter14 add auth 2

--- internal/handlers/handler_test.go
+++ internal/handlers/handler_test.go
@@ -255,7 +255,7 @@ func TestGetUrl(t *testing.T) {
 	// }
 	handler := NewHandler(memStore, cfg)
 
-	if _, err := memStore.SaveURL(id, testShortURL, testUserID, "https://testURL123.ru"); err != nil {
+	if _, err := memStore.SaveURL(id, testShortURL, "https://testURL123.ru", testUserID); err != nil {
 		require.NoError(t, err)
 	}
 
